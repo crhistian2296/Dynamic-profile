c2449466d12bab99280c4f4b808ad0c527a865ae
Crhistian Alexander Garcia Urbano <garcia.crhistian@correounivalle.edu.co>
2023-03-15 19:31:49 +0000

    first and only commit

--- src/js/app.js
+++ src/js/app.js
@@ -23,24 +23,44 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
+  const {
+    includeCover,
+    background,
+    avatarURL,
+    socialMediaPosition,
+    twitter,
+    github,
+    linkedin,
+    instagram,
+    name,
+    lastname,
+    role,
+    country,
+    city
+  } = variables;
+
+  // console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let cover = `<div class="cover"><img src="${background}" /></div>`;
+  if (includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <img src="${avatarURL}" class="photo" />
+          <h1>${name || ""} ${lastname || ""}</h1>
+          <h2>${role || ""}</h2>
+          <h3>${city || ""}, ${country || ""}</h3>
+          <ul class="${socialMediaPosition}">
+            <li><a href="https://twitter.com/${twitter ||
+              ""}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github ||
+              ""}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${linkedin ||
+              ""}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram ||
+              ""}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -60,15 +80,15 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
+    name: "",
+    lastname: "",
+    role: "",
+    country: "",
+    city: ""
   };
   render(window.variables); //render the card for the first time
 
